74c122d632d5d7beb90dbd76ca9cebe1a9246267
Crhistian Cardona <crhisto@gmail.com>
2023-10-21 18:50:28 +0000

    Modification and test of global marker use in VL

--- DecoFlexR/vignettes/background_scripts/deco_VL.merge1_global_markers.R
+++ DecoFlexR/vignettes/background_scripts/deco_VL.merge1_global_markers.R
@@ -13,14 +13,56 @@ hierarchical_clustering_VL.merge1 <- list(
   )
 )
 
+list_cell_types_VL.orig.celltype <- c('OPC', 'Endothelial', 'IN-VIP', 'L5/6-CC', 'L4', 'IN-PV', 'L2/3', 'Oligodendrocytes', 'AST-PP', 'Microglia', 'Neu-mat', 'IN-SV2C', 'L5/6', 'IN-SST', 'Neu-NRGN-I', 'AST-FB', 'Neu-NRGN-II')
+
+hierarchical_clustering_VL.total.v2.1.top.names <- c('OPC', 'Endothelial', 'Oligodendrocytes', 'Microglia', 'Astrocytes', 'Neurons')
+hierarchical_clustering_VL.total.v2.1 <- list(
+  '1'=list(tree_level=0, leaf_number=1, celltype_list = list_cell_types_VL.orig.celltype, 
+           next_level_clustering=list(
+             '1'=list(tree_level=1, leaf_number=1, name='OPC', celltype_list=c('OPC'), next_level_clustering=NULL), 
+             '2'=list(tree_level=1, leaf_number=2, name='Endothelial', celltype_list=c('Endothelial'), next_level_clustering=NULL),
+             '3'=list(tree_level=1, leaf_number=3, name='Oligodendrocytes', celltype_list=c('Oligodendrocytes'), next_level_clustering=NULL),
+             '4'=list(tree_level=1, leaf_number=4, name='Microglia', celltype_list=c('Microglia'), next_level_clustering=NULL),
+             '5'=list(tree_level=1, leaf_number=5, name='Astrocytes', celltype_list=c('AST-FB', 'AST-PP'), next_level_clustering=NULL),
+             '6'=list(tree_level=1, leaf_number=6, name='Neurons', celltype_list=c('L2/3', 'L4', 'L5/6', 'L5/6-CC', 'Neu-mat', 'IN-PV', 'IN-SST', 'IN-SV2C', 'IN-VIP'), next_level_clustering=list(
+               '1'=list(tree_level=2, leaf_number=1, name='Neurons_Exc', celltype_list=c('L2/3', 'L4', 'L5/6', 'L5/6-CC', 'Neu-mat'), next_level_clustering=NULL), 
+               '2'=list(tree_level=2, leaf_number=2, name='Neurons_Inh', celltype_list=c('IN-PV', 'IN-SST', 'IN-SV2C', 'IN-VIP'), next_level_clustering=NULL)
+             )
+             )
+           )
+  )
+)
+
+
+#All cell types: neurons groupped differently. Neu-mat is now gropped with the rest of Neu
+hierarchical_clustering_VL.total.v4 <- list(
+  '1'=list(tree_level=0, leaf_number=1, celltype_list = list_cell_types_VL.orig.celltype, 
+           next_level_clustering=list(
+             '1'=list(tree_level=1, leaf_number=1, name='OPC', celltype_list=c('OPC'), next_level_clustering=NULL), 
+             '2'=list(tree_level=1, leaf_number=2, name='Endothelial', celltype_list=c('Endothelial'), next_level_clustering=NULL),
+             '3'=list(tree_level=1, leaf_number=3, name='Oligodendrocytes', celltype_list=c('Oligodendrocytes'), next_level_clustering=NULL),
+             '4'=list(tree_level=1, leaf_number=4, name='Microglia', celltype_list=c('Microglia'), next_level_clustering=NULL),
+             '5'=list(tree_level=1, leaf_number=5, name='Astrocytes', celltype_list=c('AST-FB', 'AST-PP'), next_level_clustering=NULL),
+             '6'=list(tree_level=1, leaf_number=6, name='Neurons', celltype_list=c('L2/3', 'L4', 'L5/6', 'L5/6-CC', 'IN-PV', 'IN-SST', 'IN-SV2C', 'IN-VIP', 'Neu-mat', 'Neu-NRGN-I', 'Neu-NRGN-II'), next_level_clustering=list(
+               '1'=list(tree_level=2, leaf_number=1, name='Neurons_Exc_Inh', celltype_list=c('L2/3', 'L4', 'L5/6', 'L5/6-CC', 'IN-PV', 'IN-SST', 'IN-SV2C', 'IN-VIP'), next_level_clustering=list(
+                 '1'=list(tree_level=3, leaf_number=1, name='Neurons_Exc', celltype_list=c('L2/3', 'L4', 'L5/6', 'L5/6-CC'), next_level_clustering=NULL), 
+                 '2'=list(tree_level=3, leaf_number=2, name='Neurons_Inh', celltype_list=c('IN-PV', 'IN-SST', 'IN-SV2C', 'IN-VIP'), next_level_clustering=NULL)
+               )),
+               '2'=list(tree_level=2, leaf_number=2, name='Neurons_NRGN', celltype_list=c('Neu-mat', 'Neu-NRGN-I', 'Neu-NRGN-II'), next_level_clustering=NULL)
+             )
+             )
+           )
+  )
+)
+
 
 library(DecoFlex)
 
 #Parameters for the simulation
 single_cell_data_exp <- eset.sc.sparse_VL
-hierarchy = hierarchical_clustering_VL.merge1$`1`
+hierarchy = hierarchical_clustering_VL.total.v4$`1`
 bulk_data = data.frame(as.matrix(eset.sparse_pseudobulk_VL@assayData$exprs))
-sub_clusters_var <- 'merge1'
+sub_clusters_var <- 'orig.celltype'
 sample <- 'Disorder' #just one value: Control
 use_min_cor_strategy = TRUE
 delete_shared_level_markers = FALSE
